c4ed0f8a741f3369f927f9ed7eac71f2efa7b9c2
Ryuya Higashi <main.higashi.pg@gmail.com>
2018-05-27 22:57:27 +0900

    description改正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
       title: 'ゼロイチ',
       reverse: true,
       charset: 'utf-8',
-      description: 'エンジニアがゼロイチで何か作りたいものがあるとき要件や実装したい機能を定義していき誰かが参考URLを貼ってくれたりするサイト。どのサイトで解決したかを備忘録としてもスタックしていきましょう。ToDoにもなります。',
+      description: 'エンジニアがゼロイチで何か作りたいものがあるとき要件や実装したい機能を定義していき誰かが参考URLを貼ってくれたりする独習者支援Webサービス。どのサイトで解決したかを備忘録としてもスタックしていきましょう。ToDoにもなります。',
       keywords: 'URL共有,タスク管理,ゼロイチ,一緒に勉強,プログラミング,プロジェクト TODO',
       canonical: request.original_url,
       separator: '|',
@@ -16,7 +16,7 @@ module ApplicationHelper
       og: {
         site_name: 'Bibouroku',
         title: 'ゼロイチ',
-        description: 'エンジニアゼロイチで何か作りたいものがあるとき要件や実装したい機能を定義していき誰かが参考URLを貼ってくれたりするサイト。どのサイトで解決したかを備忘録としてもスタックしていきましょう。ToDoにもなります。',
+        description: 'エンジニアゼロイチで何か作りたいものがあるとき要件や実装したい機能を定義していき誰かが参考URLを貼ってくれたりする独習者支援Webサービス。どのサイトで解決したかを備忘録としてもスタックしていきましょう。ToDoにもなります。',
         type: 'website',
         url: request.original_url,
         image: image_url(''),
